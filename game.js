f85b15608277369145160cd3153fa2aee91e9c71
Lucia Machado <lucia.machado.olascoaga@gmail.com>
2023-05-23 14:27:21 -0300

    Finalizacion del canvas

--- game.js
+++ game.js
@@ -1,8 +1,34 @@
-const canvas = document.querySelector('#game')
-const game = canvas.getContext('2d')
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame)
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  elementsSize = canvasSize / 10;
+
+  startGame();
+}
 
 function startGame() {
-    game.fillRect(0,0,100,100)
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  }
 }
